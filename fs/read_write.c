db47e9c8b7a657e8b6860f32e9505e64aaf16f81
xCaptaiN09 <dilsheddilu123@gmail.com>
2026-06-18 20:08:37 +0530

    kernelsu: switch aosp build to manual hooks

--- fs/read_write.c
+++ fs/read_write.c
@@ -610,8 +610,15 @@ ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
+extern void ksu_handle_sys_read(unsigned int fd);
+#endif
+
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
+#ifdef CONFIG_KSU
+	ksu_handle_sys_read(fd);
+#endif
 	return ksys_read(fd, buf, count);
 }
 
