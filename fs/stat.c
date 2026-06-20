4f595bee4a7d84b71adfcea6a7ba839f84b474a2
xCaptaiN09 <dilsheddilu123@gmail.com>
2026-06-21 00:10:59 +0530

    fix: add missing fstat64_ret prototype to stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -21,6 +21,7 @@
 #include <linux/vndfs_def.h>
 #endif
 
+extern void vnd_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr);
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
 
@@ -28,7 +29,6 @@
 extern void vnd_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);
 #endif
 #ifdef CONFIG_KSU_VNDFS_SUS_SU
-extern void vnd_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr);
 #endif
 #ifdef CONFIG_KSU_VNDFS_SUS_KSTAT
 extern void vndfs_sus_ino_for_generic_fillattr(unsigned long ino, struct kstat *stat);
