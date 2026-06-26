126a96f53d1dd743f9f47de49dd4dbfba2eb88c8
xCaptaiN09 <dilsheddilu123@gmail.com>
2026-06-26 10:20:09 +0530

    fix: keep selinux attr current filtering targeted

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -6378,7 +6378,8 @@ static int selinux_setprocattr(const char *name, void *value, size_t size)
 	else
 		error = -EINVAL;
 
-	if (!strcmp(name, "current") && selinux_is_app_uid())
+	if (!strcmp(name, "current") &&
+	    selinux_should_hide_root_context(value, size))
 		return -EINVAL;
 
 	if (error)
